9dd1621a67dae31b908962e447d4a88e00030ab9
Carl <Carl.Kamholtz@gmail.com>
2021-04-27 22:03:26 +1000

    60s timeout + don't turn lights on unless presence just went from false to true

--- index.js
+++ index.js
@@ -20,42 +20,46 @@ Promise.all([tradfri.connect(), v3.discovery.nupnpSearch()])
 	})
 	.then(api => {
 		const PRECENCE_SENSOR_ID = 33;
-		const TIMEOUT_MAX = 5;
+		const TIMEOUT_MAX = 60;
 		const KITCHEN_LIGHTS = tradfri.devices.filter(x => x.name.match(/K\d/));
 
 		var timeout = 0;
 		var seconds = 0;
+		var wasPresent = false;
 
 		function AnyKitchenLightsOn(kitchenLights) {
 			return kitchenLights.some(x => x.isOn);
 		}
 
 		setInterval(async () => {
+			// Sensing
 			const presenceSensor = await api.sensors.getSensor(PRECENCE_SENSOR_ID);
 			var initialLightsOn = AnyKitchenLightsOn(KITCHEN_LIGHTS);
 
-			console.log("ON? : " + initialLightsOn);
 			if (presenceSensor.presence) {
 				timeout = 0;
 			} else if (!presenceSensor.presence && initialLightsOn) {
 				timeout++;
 			}
 
+			// State update
 			if (!presenceSensor.presence && timeout > TIMEOUT_MAX && initialLightsOn) {
 				// Turn off
 				let success = await Promise.all(KITCHEN_LIGHTS.map(x => x.switch(false)));
 				if (success) console.log("Lights are off");
 
 				timeout = 0;
-			} else if (presenceSensor.presence && !initialLightsOn) {
+			} else if (!wasPresent && presenceSensor.presence && !initialLightsOn) {
 				// Turn on
 				let success = await Promise.all(KITCHEN_LIGHTS.map(x => x.switch(true)));
 				if (success) console.log("Lights are on");
 			}
 
+			// Lights on?
 			var endLightsOn = AnyKitchenLightsOn(KITCHEN_LIGHTS);
 
 			seconds++;
-			console.log(`Total seconds = ${seconds}, Timeout = ${timeout}/${TIMEOUT_MAX}, presence = ${presenceSensor.presence}, lights on? = ${endLightsOn}`);
+			wasPresent = presenceSensor.presence;
+			console.log(`Total seconds = ${seconds}, Timeout = ${timeout}/${TIMEOUT_MAX}, presence = ${presenceSensor.presence}, lights initially on? = ${initialLightsOn}, lights now on? = ${endLightsOn}`);
 		}, 1000);
 	});
